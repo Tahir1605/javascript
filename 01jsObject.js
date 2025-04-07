5d5b7f33270a8035b6f9f3113bec59c5f1244552
Tahirul Islam <5566tahirul@gmail.com>
2025-04-07 13:52:57 +0530

    javascript object 1

---
+++ 01jsObject.js
@@ -0,0 +1,73 @@
+// Create an object in javascript by using object literal {}
+// object literal nothing but a key value pair enclosed 
+// with in a {}
+// name:value pairs are also called key:value pairs.
+// object literals are also called object initializers.
+
+// declaration -> 1
+
+// const persion = {
+//     name : "Tahirul islam",
+//     age : 23,
+//     city : "memari",
+// }
+
+// console.log(person);
+
+// declaration -> 2
+
+// const persion = {}
+
+// persion.name = "Tahirul islam"
+// persion.age = 23
+// persion.city = "memari"
+
+// console.log(persion);
+
+// Create javascript object by using new keyword
+
+// const persion = new Object()
+
+// persion.name = "Tahirul islam"
+// persion.age = 23
+// persion.city = "memari"
+
+// console.log(persion);
+
+
+// Accessing Object Properties
+
+// const persion = {
+//     name:{
+//         firstName:"Tahirul",
+//         lastName:"islam"
+//     },
+//     age:23,
+//     city:"memari"
+// }
+
+// console.log(persion.name);
+// console.log(persion.age);
+// console.log(persion.name.firstName);
+
+
+// javascript objects are mutable
+
+const persion = {
+    name:{
+        firstName:"Tahirul",
+        lastName:"islam"
+    },
+    age:23,
+    city:"memari"
+}
+console.log(persion.city);
+persion.city = "kolkata"
+console.log(persion.city);
+
+
+
+
+
+
+
